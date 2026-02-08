1082b4d6d18b23dd7a448d503cc7cfa3b3eb06b1
Alexistodj124 <55223401+Alexistodj124@users.noreply.github.com>
2026-02-08 00:22:36 -0600

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://portal_user:Alexis2012@localhost:5432/Libertad")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://portal_user:Alexis2012@localhost:5432/portal_db")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
